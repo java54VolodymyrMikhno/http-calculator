78bebf4ecb32c37bcaa58c029c294e8fff0811d1
VolodymyrMikhno <volodymyr3583@gmail.com>
2024-11-21 22:51:42 +0200

    Updating CalculatorService

--- src/service/CalculatorServise.mjs
+++ src/service/CalculatorServise.mjs
@@ -3,9 +3,11 @@ import CalculatorView from "../view/CalculatorView.mjs";
 const view = new CalculatorView()
 export default class CalculatorService {
     constructor(emitter, operations) {
-       emitter.addListener('add', (operands, res) => {
-            const result = operations.get('add')(operands[0],operands[1]);
-            res.end(view.getHtml(result,false));
+        operations.forEach((operation, operationType) => {
+            emitter.addListener(operationType, (operands, res) => {
+                const result = operation(operands[0], operands[1]);
+                res.end(view.getHtml(result, false));
+        });
         });
     }
 }
